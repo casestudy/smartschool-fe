79d4b4cb75ecb1265093702a35b7ec5039c16d76
Femencha Azombo Fabrice <femenchaazombofabrice@gmail.com>
2022-10-15 21:31:18 +0100

    Completed app menu

--- src/Components/UI/Menu/AppMenu.tsx
+++ src/Components/UI/Menu/AppMenu.tsx
@@ -1,5 +1,20 @@
 import React from "react"
+import { Link } from "react-router-dom";
+
 import { Col, Row } from 'antd';
+
+import AdminIcon from '../Icons/Admin';
+import TeacherIcon from '../Icons/Teacher';
+import StudentIcon from '../Icons/Students';
+import ParentIcon from '../Icons/Parents';
+import RolesIcon from '../Icons/Roles';
+import SubjectsIcon from '../Icons/Subjects';
+import GroupsIcon from '../Icons/Groups';
+import ClassroomIcon from '../Icons/Classrooms';
+import RequestIcon from '../Icons/Requests';
+import AssistanceIcon from '../Icons/Assistances';
+import CalendarIcon from '../Icons/Calendar';
+import SettingsIcon from '../Icons/Settings';
 
 import './AppMenu.css';
   
@@ -8,16 +23,18 @@ interface Prop {
 	visibility: DocumentVisibilityState;
 }
 
-
+const styleIcon: React.CSSProperties = {paddingTop: '30px', textAlign: 'center'};
+const styleLabel: React.CSSProperties = {textAlign: 'center', fontWeight: '500', color: 'black !important'};
 
 const LeftMenu: React.FC<Prop> = ({height, visibility}) => {
     return (
         <>
             <div style={{
-					overflow: "hidden", 
+					overflowY: "scroll", 
+					overflowX: "hidden",
 					position: "absolute", 
 					top: "0px", 
-					width: "328px", 
+					width: "380px", 
 					zIndex: "911", 
 					height: `${height}`, 
 					marginTop: "57px", 
@@ -28,10 +45,67 @@ const LeftMenu: React.FC<Prop> = ({height, visibility}) => {
 					borderRadius: "15px",
 					maxHeight: "calc(-65px + 100vh)"}}>
 				
-                <Row>
-					<Col span={8} className="gutter-row">Admnis</Col>
-					<Col span={8} className="gutter-row">Teachers</Col>
-					<Col span={8} className="gutter-row">Students</Col>
+                <Row gutter={16}>
+					<Col span={8} className="gutter-row">
+						<Row justify="center" align="middle" style={styleIcon}><AdminIcon/></Row>
+						<Row justify="center" style={styleLabel}>Administrators</Row>
+					</Col>
+					<Col span={8} className="gutter-row">
+						<Row justify="center" align="middle" style={styleIcon}><TeacherIcon/></Row>
+						<Row justify="center" style={styleLabel}>Teachers</Row>
+					</Col>
+					<Col span={8} className="gutter-row">
+						<Row justify="center" align="middle" style={styleIcon}><StudentIcon/></Row>
+						<Row justify="center" style={styleLabel}>Students</Row>
+					</Col>
+				</Row>
+				<Row gutter={16}>
+					<Col span={8} className="gutter-row">
+						<Row justify="center" align="middle" style={styleIcon}><ParentIcon/></Row>
+						<Row justify="center" style={styleLabel}>Parents</Row>
+					</Col>
+					<Col span={8} className="gutter-row">
+						<Link to="/roles" className="app-menu-link">
+							<div className="app-menu-item-container">
+								<Row justify="center" align="middle" style={styleIcon}><RolesIcon/></Row>
+								<Row justify="center" style={styleLabel}>Roles</Row>
+							</div>
+							
+						</Link>
+						
+					</Col>
+					<Col span={8} className="gutter-row">
+						<Row justify="center" align="middle" style={styleIcon}><SubjectsIcon/></Row>
+						<Row justify="center" style={styleLabel}>Subjects</Row>
+					</Col>
+				</Row>
+				<Row gutter={16}>
+					<Col span={8} className="gutter-row">
+						<Row justify="center" align="middle" style={styleIcon}><GroupsIcon/></Row>
+						<Row justify="center" style={styleLabel}>Groups</Row>
+					</Col>
+					<Col span={8} className="gutter-row">
+						<Row justify="center" align="middle" style={styleIcon}><ClassroomIcon/></Row>
+						<Row justify="center" style={styleLabel}>Classrooms</Row>
+					</Col>
+					<Col span={8} className="gutter-row">
+						<Row justify="center" align="middle" style={styleIcon}><RequestIcon/></Row>
+						<Row justify="center" style={styleLabel}>Requests</Row>
+					</Col>
+				</Row>
+				<Row gutter={16} style={{paddingBottom: "30px"}}>
+					<Col span={8} className="gutter-row">
+						<Row justify="center" align="middle" style={styleIcon}><AssistanceIcon/></Row>
+						<Row justify="center" style={styleLabel}>Assistances</Row>
+					</Col>
+					<Col span={8} className="gutter-row">
+						<Row justify="center" align="middle" style={styleIcon}><CalendarIcon/></Row>
+						<Row justify="center" style={styleLabel}>Calendar</Row>
+					</Col>
+					<Col span={8} className="gutter-row">
+						<Row justify="center" align="middle" style={styleIcon}><SettingsIcon/></Row>
+						<Row justify="center" style={styleLabel}>Settings</Row>
+					</Col>
 				</Row>
             </div>
         </>
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom' 
 import reportWebVitals from './reportWebVitals';
 
+export default function MainApp() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App/>}/>
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  	document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<MainApp/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
