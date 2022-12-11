0e8fdebe9504ce00fb794183a859ff867291dfdb
Femencha Azombo Fabrice <femenchaazombofabrice@gmail.com>
2022-12-11 12:40:47 +0100

    Changes on desktop

--- src/Screens/Students/StudentScreen.tsx
+++ src/Screens/Students/StudentScreen.tsx
@@ -117,51 +117,50 @@ const StudentScreen: React.FC<any> = () => {
     ].filter(item => !item.hidden);
 
 	useEffect(() => {
-        // const b64 : any = localStorage.getItem('data');
-        // const store : any = base64_decode(b64) ;
-        // const locale = JSON.parse(store).result.value[0][0].locale;
+        const b64 : any = localStorage.getItem('data');
+        const store : any = base64_decode(b64) ;
+        const locale = JSON.parse(store).result.value[0][0].locale;
 
-		// const data = {
-		// 	connid: localStorage.getItem('connid'),
-        //     utype: 'teacher',
-        //     locale: locale
-		// };
+		const data = {
+			connid: localStorage.getItem('connid'),
+			locale: locale
+		};
 
-		// dispatch(fetchUsersAsync(data)).then((value) => {
-		// 	const result = value.payload ;
-		// 	//console.log(result);
-		// 	if(result.error === false) {
-		// 		// We have the db results here
-		// 		const dataSource = result.result.value;
-		// 		setFilteredTeachers(dataSource);
-		// 		setOriginalTeachers(dataSource);
-		// 		setLoading(false);
-		// 	} else {
-		// 		//An axios error
-		// 		let msg = '';
-		// 		let code = '';
+		dispatch(fetchStudentsAsync(data)).then((value) => {
+			const result = value.payload ;
+			//console.log(result);
+			if(result.error === false) {
+				// We have the db results here
+				const dataSource = result.result.value;
+				setFilteredStudents(dataSource);
+				setOriginalStudents(dataSource);
+				setLoading(false);
+			} else {
+				//An axios error
+				let msg = '';
+				let code = '';
 	
-		// 		if(result.status === 400) {
-		// 			msg = result.message;
-		// 			code = result.code;
-		// 		} else {
-		// 			//It is error from the back end
-		// 			msg = result.error.msg;
-		// 			code = result.error.code;
-		// 		}
-		// 		const modal = Modal.error({
-		// 			title: `Teachers`,
-		// 			content: msg + ' (' + code + ')',
-		// 			icon: <Danger color={Color.teachers}/>
-		// 		});
+				if(result.status === 400) {
+					msg = result.message;
+					code = result.code;
+				} else {
+					//It is error from the back end
+					msg = result.error.msg;
+					code = result.error.code;
+				}
+				const modal = Modal.error({
+					title: `Students`,
+					content: msg + ' (' + code + ')',
+					icon: <Danger color={Color.students}/>
+				});
 	
-		// 		modal.update({});
-		// 		setLoading(false);
-		// 	}
-		// },(error) => {
-		// 	console.log("Error");
-		// 	console.log(error);
-		// } );
+				modal.update({});
+				setLoading(false);
+			}
+		},(error) => {
+			console.log("Error");
+			console.log(error);
+		} );
 	}, [])
 
     return (
